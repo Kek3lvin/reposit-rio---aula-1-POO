c89dffe0a7c13b9ddbdc363dffb52667a4afaf27
Kek3lvin <franca.kelvin@escolar.ifrn.edu.br>
2025-04-15 14:43:29 +0000

    Algumas questões

--- Aula-01/Ah.py
+++ Aula-01/Ah.py
@@ -2,4 +2,6 @@ print("Kelvin de França Ferreira")
 print('Matrícula: 20241011110019')
 
 nome = input('Digite aqui seu primeiro nome:')
-print(f'Bem-vindo(a) ao Codespaces, {nome}')
+print(f'Bem-vindo(a) ao Codespaces,{nome}')
+
+#REVISÃO
---
+++ Aula-2/Rev_lista2.py
@@ -0,0 +1,4 @@
+#QUESTÃO 1
+print('Digite seu nome abaixo:')
+nome = input()
+print(f'Bem-vindo(a) ao Python, {nome.split()[0]}')
