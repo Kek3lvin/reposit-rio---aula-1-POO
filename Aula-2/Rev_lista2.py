ffd4bfb8879a5162cc27c00b3fe6a7201fa0bb99
Kek3lvin <franca.kelvin@escolar.ifrn.edu.br>
2025-04-22 13:28:43 +0000

    k

--- Aula-2/Rev_lista2.py
+++
@@ -1,4 +0,0 @@
-#QUESTÃO 1
-print('Digite seu nome abaixo:')
-nome = input()
-print(f'Bem-vindo(a) ao Python, {nome.split()[0]}')
---
+++ Aula-2/Rev_lista3.py
@@ -0,0 +1,17 @@
+#QUESTÃO 1
+a = int(input())
+b = int(input())
+
+x = a + b
+
+print(f'X = {x}')
+
+#QUESTÃO 2
+a = float(input()) 
+b = float(input())
+
+MEDIA = (a * 3.5 +  b * 7.5) / 11
+
+print(f'MEDIA = {MEDIA:.5f}')
+
+#QUESTÃO 3
